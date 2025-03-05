4f5dcd080a1af99651760ffa1d461ae44a81e9f7
Michael Blow <michael.blow@couchbase.com>
2025-03-05 07:16:27 -0500

    MB-65529: add missing files
    Change-Id: I5ce83b89725167232a4b773ce287b51c0d679c26
    Reviewed-on: https://review.couchbase.org/c/columnar-ui-v1/+/224410
    Tested-by: Michael Blow <michael.blow@couchbase.com>
    Reviewed-by: Michael Blow <michael.blow@couchbase.com>

---
+++ src/ui/app/mn.columnar.bucket.config.component.js
@@ -0,0 +1,44 @@
+/*
+Copyright 2020-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
+import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {takeUntil, pluck, distinctUntilChanged} from 'rxjs/operators';
+import {BehaviorSubject} from 'rxjs';
+
+import {MnPoolsService} from './mn.pools.service.js';
+import {MnWizardService} from './mn.wizard.service.js';
+import {MnLifeCycleHooksToStream} from './mn.core.js';
+import template from "./mn.columnar.bucket.config.html"
+
+export {MnColumnarbucketConfig};
+
+class MnColumnarbucketConfig extends MnLifeCycleHooksToStream {
+  static get annotations() { return [
+    new Component({
+      selector: "mn-columnar-bucket-config",
+      template,
+      inputs: [
+        "group",
+      ],
+      changeDetection: ChangeDetectionStrategy.OnPush
+    })
+  ]}
+
+  static get parameters() { return [
+    MnWizardService,
+  ]}
+
+  constructor(mnWizardService) {
+    super();
+    this.focusFieldSubject = new BehaviorSubject(true);
+    this.postNodeInitHttp = mnWizardService.stream.postNodeInitHttp;
+    this.postClusterInitHttp = mnWizardService.stream.postClusterInitHttp;
+  }
+}
